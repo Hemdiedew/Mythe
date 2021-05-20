20e75a9810ef398d7a055ff2dd387e802b6a73e2
Jaimy Koster <43574560+RekCus@users.noreply.github.com>
2021-05-20 11:24:51 +0200

    HP Bar for enemies
    HP bar voor enemies is gemaakt, de healthbar werkt maar er is geen code die  de enemy shade geeft, de health variable is in int maar kan naar float verandert worden voor de implementatie

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -28,7 +28,7 @@ public class EnemyController : MonoBehaviour
             if(distance <= agent.stoppingDistance)
             {
                 FaceTarget();
-                // Attack Player
+                //EnemyStats.AttackPlayer();
             }
         }
 
---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    int maxHealth;
+    int currentHealth;
+    int damage;
+
+    public Healthbar healthbar;
+
+    public bool EnemyType1;
+    //public bool EnemyType2;
+    //public bool EnemyType3;
+
+    private void Start()
+    {
+        if(EnemyType1 == true)
+        {
+            maxHealth = 10;
+            damage = 5;
+        }
+
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    void Attack()
+    {
+        //Deals Damage to player based on "damage"
+    }
+
+    void Hurt()
+    {
+        //Lowers hp by Player Attacks damage
+        //Play Hurt Animation
+
+        healthbar.SetHealth(currentHealth);
+    }
+
+    void Die()
+    {
+        //Play Death Animation
+        Destroy(gameObject);
+    }
+
+}
